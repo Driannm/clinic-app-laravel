6883522a1f08392f4e1d54d4785f49dbec1a1ef1
Driann's <114148680+Driannm@users.noreply.github.com>
2024-10-22 14:26:50 +0700

    Update: add search feature

--- resources/views/Pasien/pasien_index.blade.php
+++ resources/views/Pasien/pasien_index.blade.php
@@ -1,50 +1,65 @@
 @extends('mylayout', ['title' => 'Data Pasien'])
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
-    <div class="card">
-      <h5 class="card-header">Data Pasiens</h5>
-      <div class="table-responsive text-nowrap">
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th>No</th>
-              <th>No Pasien</th>
-              <th>Foto</th>
-              <th>Nama</th>
-              <th>Umur</th>
-              <th>Jenis Kelamin</th>
-              <th>Tanggal Input</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody class="table-border-bottom-0">
-            @forelse ($pasien as $item)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $item->no_pasien }}</td>
-                <td>
-                  <div class="showPhoto">
-                    <div id="imagePreview" style="@if ($item->foto != '') background-image:url('{{ url('/') }}/uploads/{{ $item->foto }}')
-                    @else background-image: url('{{ url('/storage/avatar.png') }}') @endif;">
-                    </div>
-                </div>
-                </td>
-                <td>{{ $item->nama }}</td>
-                <td>{{ $item->umur }}</td>
-                <td>{{ $item->jenis_kelamin }}</td>
-                <td>{{ $item->created_at }}</td>
-                <td><b>Coming Soon!</b></td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="8">Tidak Ada Data Pasien</td>
-            </tr>
-            @endforelse
-          </tbody>
-        </table>
+  <div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+      <h5 class="mb-0">Data Pasiens</h5>
+      <!-- Pindahin Search Bar ke sini -->
+      <div class="navbar-nav align-items-center">
+          <div class="nav-item d-flex align-items-center">
+              <i class="bx bx-search bx-md"></i>
+              <input
+                  type="text"
+                  class="form-control border-0 shadow-none ps-1 ps-sm-2"
+                  placeholder="Search..."
+                  aria-label="Search..." />
+          </div>
       </div>
     </div>
+
+    <div class="table-responsive text-nowrap">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>No Pasien</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Jenis Kelamin</th>
+            <th>Tanggal Input</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          @forelse ($pasien as $item)
+          <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td>{{ $item->no_pasien }}</td>
+              <td>
+                <div class="showPhoto">
+                  <div id="imagePreview" style="@if ($item->foto != '') background-image:url('{{ url('/') }}/uploads/{{ $item->foto }}')
+                  @else background-image: url('{{ url('/storage/avatar.png') }}') @endif;">
+                  </div>
+              </div>
+              </td>
+              <td>{{ $item->nama }}</td>
+              <td>{{ $item->umur }}</td>
+              <td>{{ $item->jenis_kelamin }}</td>
+              <td>{{ $item->created_at }}</td>
+              <td><b>Coming Soon!</b></td>
+          </tr>
+          @empty
+          <tr>
+              <td colspan="8">Tidak Ada Data Pasien</td>
+          </tr>
+          @endforelse
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
+
 @endsection	
 
 <style>

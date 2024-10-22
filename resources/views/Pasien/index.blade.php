1aee2d971fe17b196296e9c8a2fa435b90b4fc5a
Driann's <114148680+Driannm@users.noreply.github.com>
2024-10-22 17:50:04 +0700

    Update: change folder name

---
+++ resources/views/Pasien/index.blade.php
@@ -0,0 +1,87 @@
+@extends('mylayout', ['title' => 'Daftar Pasien'])
+@section('content')
+<div class="container-xxl flex-grow-1 container-p-y">
+  <div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+      <h5 class="mb-0">Data Pasiens</h5>
+      <!-- Pindahin Search Bar ke sini -->
+      <div class="navbar-nav align-items-center">
+          <div class="nav-item d-flex align-items-center">
+              <i class="bx bx-search bx-md"></i>
+              <input
+                  type="text"
+                  class="form-control border-0 shadow-none ps-1 ps-sm-2"
+                  placeholder="Search..."
+                  aria-label="Search..." />
+          </div>
+      </div>
+    </div>
+
+    <div class="table-responsive text-nowrap">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>No Pasien</th>
+            <th>Foto</th>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Jenis Kelamin</th>
+            <th>Tanggal Input</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          @forelse ($pasien as $item)
+          <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td>{{ $item->no_pasien }}</td>
+              <td>
+                <div class="showPhoto">
+                  <div id="imagePreview" style="@if ($item->foto != '') background-image:url('{{ url('/') }}/uploads/{{ $item->foto }}')
+                  @else background-image: url('{{ url('/storage/avatar.png') }}') @endif;">
+                  </div>
+              </div>
+              </td>
+              <td>{{ $item->nama }}</td>
+              <td>{{ $item->umur }}</td>
+              {{-- <td>{{ $item->jenis_kelamin }}</td> --}}
+              <td>{{ ucfirst($item->jenis_kelamin) }}</td>
+              <td>{{ $item->created_at }}</td>
+              <td>
+                <a href="/pasien/{{ $item -> id }}/edit" class="btn btn-info mt-1">Edit</a>
+                <a href="/pasien/detail/{{ $item -> id }}" class="btn btn-primary mt-1">Detail</a>
+                <a href="/pasien/{{ $item -> id }}/edit" class="btn btn-danger mt-1">Hapus</a>
+              </td>
+          </tr>
+          @empty
+          <tr>
+              <td colspan="8">Tidak Ada Data Pasien</td>
+          </tr>
+          @endforelse
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+@endsection	
+
+<style>
+  .showPhoto {
+      width: 80%;
+      height: 45px;
+      display: flex;
+      justify-content: flex-start; /* Foto akan ke kiri */
+      align-items: center;
+  }
+
+  .showPhoto > div {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+  }
+</style>

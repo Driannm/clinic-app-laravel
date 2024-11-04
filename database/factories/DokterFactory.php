511f46845169c363d1bf1b38168edb474563e62d
Driann's <114148680+Driannm@users.noreply.github.com>
2024-11-05 00:23:24 +0700

    update: add DokterFactory

---
+++ database/factories/DokterFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dokter>
+ */
+class DokterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+                'no_dokter' => fake() -> unique() -> randomNumber(8),
+                'nama' => 'Dr. ' . fake()->name() . ', Sp.' . fake()->randomElement(['A', 'B', 'D', 'J', 'P', 'T']),
+                'umur' => fake() -> numberBetween(20, 25),
+                'jenis_kelamin' => fake() -> randomElement(['Laki-Laki', 'Perempuan']),
+                'kategori' => fake() -> randomElement(['Umum', 'Spesialis Anak', 'Spesialis Bedah', 'Spesialis Jantung', 'Spesialis Kulit']),
+                'keahlian' => fake() -> randomElement(['Bedah Umum', 'Kardiologi', 'Pediatri', 'Ortopedi', 'Dermatologi']),
+        ];
+    }
+}

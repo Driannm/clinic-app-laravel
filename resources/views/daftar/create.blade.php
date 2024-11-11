f896f348fc033e2ee3e5625f2169fd7316e0291b
Driann's <114148680+Driannm@users.noreply.github.com>
2024-11-11 20:14:46 +0700

    update: add select2 to daftar.create

--- resources/views/daftar/create.blade.php
+++ resources/views/daftar/create.blade.php
@@ -15,49 +15,61 @@
                 @endif
                 @include('flash::message')
 
-                <form action="/daftar/create" method="POST"> 
-                    @csrf 
-                    <div class="form-group mt-3"> 
-                        <label for="tanggal_daftar" class="form-label">Tanggal Daftar</label> 
-                        <input type="date" name="tanggal_daftar" class="form-control" 
-                            value="{{ old('tanggal_daftar') ?? date('Y-m-d') }}"> 
-                        <span class="text-danger">{{ $errors->first('tanggal_daftar') }}</span> 
-                    </div> 
-                    <div class="form-group mt-3"> 
-                        <label for="pasien_id" class="form-label">Nama Pasien</label> 
-                        <select name="pasien_id" class="form-control"> 
-                            <option value="">-- Pilih Pasien --</option> 
-                            @foreach ($listPasien as $item) 
-                                <option value="{{ $item->id }}" @selected(old('pasien_id') == $item->id)> 
-                                    {{ $item->nama }} 
-                                </option> 
-                            @endforeach 
-                        </select> 
-                        <span class="text-danger">{{ $errors->first('pasien_id') }}</span> 
-                    </div> 
+                <form action="/daftar/create" method="POST">
+                    @csrf
+                    <div class="form-group mt-3">
+                        <label for="tanggal_daftar" class="form-label">Tanggal Daftar</label>
+                        <input type="date" name="tanggal_daftar" class="form-control"
+                            value="{{ old('tanggal_daftar') ?? date('Y-m-d') }}">
+                        <span class="text-danger">{{ $errors->first('tanggal_daftar') }}</span>
+                    </div>
+                    <div class="form-group mt-3">
+                        <label for="pasien_id" class="form-label">Nama Pasien</label>
+                        <select name="pasien_id" class="form-control select2">
+                            <option value="">-- Pilih Pasien --</option>
+                            @foreach ($listPasien as $item)
+                                <option value="{{ $item->id }}" @selected(old('pasien_id') == $item->id)>
+                                    {{ $item->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <span class="text-danger">{{ $errors->first('pasien_id') }}</span>
+                    </div>
                     {{-- Poli --}}
-                    <div class="form-group mt-3"> 
-                        <label for="poli_id" class="form-label">Poli</label> 
-                        <select name="poli_id" class="form-control"> 
-                            <option value="">-- Pilih Poli --</option> 
-                            @foreach ($listPoli as $item) 
-                                <option value="{{ $item->id }}" @selected(old('pasien_id') == $item->id)> 
-                                    {{ $item->nama }} 
-                                </option> 
-                            @endforeach 
-                        </select> 
-                        <span class="text-danger">{{ $errors->first('poli') }}</span> 
-                    </div> 
+                    <div class="form-group mt-3">
+                        <label for="poli_id" class="form-label">Poli</label>
+                        <select name="poli_id" class="form-control select2">
+                            <option value="">-- Pilih Poli --</option>
+                            @foreach ($listPoli as $item)
+                                <option value="{{ $item->id }}" @selected(old('poli_id') == $item->id)>
+                                    {{ $item->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <span class="text-danger">{{ $errors->first('poli') }}</span>
+                    </div>
                     {{-- Keluhan --}}
-                    <div class="form-group mt-3 mb-3"> 
-                        <label for="keluhan" class="form-label">Keluhan</label> 
-                        <textarea name="keluhan" rows="2" class="form-control">{{ old('keluhan') }}</textarea> 
-                        <span class="text-danger">{{ $errors->first('keluhan') }}</span> 
-                    </div> 
-                    <button type="submit" class="btn btn-primary">Simpan</button> 
+                    <div class="form-group mt-3 mb-3">
+                        <label for="keluhan" class="form-label">Keluhan</label>
+                        <textarea name="keluhan" rows="2" class="form-control">{{ old('keluhan') }}</textarea>
+                        <span class="text-danger">{{ $errors->first('keluhan') }}</span>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
                 </form>
-
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.select2').select2({
+                placeholder: "Pilih salah satu",
+                allowClear: true
+            });
+        });
+    </script>
 @endsection
